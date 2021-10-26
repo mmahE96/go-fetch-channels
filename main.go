e53455a79719ce817d02c0658d9c2fa7ca7e488e
Mahir Mulaomerovic <mahir@HUAWEI_Mate_10_lite-8694b.localdomain>
2021-10-26 12:46:27 +0200

    Added fethcing pause of 5 minutes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -20,13 +21,18 @@ func main() {
 		go makeRequest(link, c)
 
 	}
-	for {
-		go makeRequest(<-c, c)
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			makeRequest(link, c)
+		}(l)
+
 	}
 
 }
 
 func makeRequest(link string, c chan string) {
+
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Printf(link, "might be down")
